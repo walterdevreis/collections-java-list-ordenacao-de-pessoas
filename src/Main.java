cc7faa8af8a63212db9a1813db61d5ff26234346
walter dev reis <walter-reis@outlook.com>
2025-09-17 15:42:28 -0300

    Criada e testada a classe Pessoa

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import entity.Pessoa;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Pessoa pessoa = new Pessoa("Pedro", 50, 1.80);
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getAltura());
+        System.out.println(pessoa.getIdade());
+
+        System.out.println("------------------");
+
+        System.out.println(pessoa);
+
     }
 }
